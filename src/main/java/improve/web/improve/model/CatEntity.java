f33c1b0350cf1b01594b188a98af1becee4800f7
uev <uev5bsd@gmail.com>
2016-09-30 22:41:00 +0700

    Удаление лишнего кода. Аннотация Data формирует для бинов всё необходимое

--- src/main/java/improve/web/improve/model/CatEntity.java
+++ src/main/java/improve/web/improve/model/CatEntity.java
@@ -1,8 +1,6 @@
 package improve.web.improve.model;
 
 import lombok.Data;
-
-
 import javax.persistence.*;
 import java.lang.Long;
 
@@ -21,23 +19,4 @@ public @Data class CatEntity {
     @Column(name = "name")
     private String name;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CatEntity catEntity = (CatEntity) o;
-
-        if (id != null ? !id.equals(catEntity.id) : catEntity.id != null) return false;
-        if (name != null ? !name.equals(catEntity.name) : catEntity.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
 }
--- src/main/java/improve/web/improve/model/ProdEntity.java
+++ src/main/java/improve/web/improve/model/ProdEntity.java
@@ -1,7 +1,6 @@
 package improve.web.improve.model;
 
 import lombok.Data;
-
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -14,26 +13,4 @@ public @Data class  ProdEntity {
     @Id @Column(name = "id") private Long id;
     @Basic @Column(name = "name") private String name;
     @Basic @Column(name = "price") private Double price;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ProdEntity that = (ProdEntity) o;
-
-        if (id != null ? id.equals(that.id) : that.id != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (price != null ? !price.equals(that.price) : that.price != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (price != null ? price.hashCode() : 0);
-        return result;
-    }
 }
