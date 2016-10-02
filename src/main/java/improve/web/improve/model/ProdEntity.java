73a22aa405c49bf1c229762c31a33c69b39c8505
uev <uev5bsd@gmail.com>
2016-10-02 22:38:15 +0700

    Изменение модели

--- src/main/java/improve/web/improve/model/ProdEntity.java
+++ src/main/java/improve/web/improve/model/ProdEntity.java
@@ -14,4 +14,6 @@ public @Data class  ProdEntity implements Serializable {
     @Id @Column(name = "id") private Long id;
     @Basic @Column(name = "name") private String name;
     @Basic @Column(name = "price") private Double price;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)  @JoinColumn(name = "cat_id")
+    private CatEntity categoryFk;
 }
