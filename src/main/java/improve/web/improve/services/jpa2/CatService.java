94fa6225135d8b405c5d733fb5a53747e5714a3d
uev <uev5bsd@gmail.com>
2016-10-06 20:50:06 +0700

    Фильтр.

--- src/main/java/improve/web/improve/services/jpa2/CatService.java
+++ src/main/java/improve/web/improve/services/jpa2/CatService.java
@@ -1,12 +1,16 @@
 package improve.web.improve.services.jpa2;
 
-import improve.web.improve.model.CatEntity;
+import com.mysema.query.BooleanBuilder;
+import com.mysema.query.jpa.JPQLQuery;
+import com.mysema.query.types.expr.SimpleExpression;
 import improve.web.improve.model.QCatEntity;
+import improve.web.improve.model.QProdEntity;
 import improve.web.improve.services.tomcat.EntityManagerContext;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by uev on 24.09.16.
@@ -16,9 +20,34 @@ import java.util.List;
 @ApplicationScoped
 public class CatService {
 
-    public List<CatEntity> getList() {
-        QCatEntity catEntity = QCatEntity.catEntity;
-        List<CatEntity> result = EntityManagerContext.getJPQL().from(catEntity).fetchAll().list(catEntity);
-        return result;
+    public JPQLQuery getJPQLQuery() {
+        return EntityManagerContext.getJPQL();
+    }
+
+    public Object[] getList(Object... params) {
+        final QCatEntity catEntity = QCatEntity.catEntity;
+        final QProdEntity prodEntity = QProdEntity.prodEntity;
+        Map<String,SimpleExpression> filterArgs = new HashMap<String, SimpleExpression>() {{
+            put("priceOf",prodEntity.price);
+            put("priceTo", prodEntity.price);
+            put("category", catEntity.name);
+            put("product", prodEntity.name);
+        }};
+        BooleanBuilder builder = new BooleanBuilder();
+        JPQLQuery jpql = getJPQLQuery();
+        jpql.from(prodEntity).join(prodEntity.categoryFk, catEntity);
+
+        if (params.length > 0) {
+            Map<String, String> args = (Map<String, String>) params[0];
+            for (Map.Entry<String,SimpleExpression> entry: filterArgs.entrySet()) {
+                if (args.containsKey(entry.getKey())) {
+                    String compValue = args.get(entry.getKey()).toString();
+                    builder.and(entry.getValue().eq(compValue));
+                }
+            }
+            jpql.where(builder);
+        }
+        Object[] dlst = jpql.list(prodEntity) .toArray();
+        return dlst;
     }
 }
--- src/test/java/improve/model/hibernate/SessionFactoryTest.java
+++ src/test/java/improve/model/hibernate/SessionFactoryTest.java
@@ -1,16 +1,23 @@
 package improve.model.hibernate;
 
+import static org.mockito.Mockito.*;
+
+import com.google.common.collect.ImmutableMap;
 import com.mysema.query.jpa.JPQLQuery;
 import com.mysema.query.jpa.impl.JPADeleteClause;
 import com.mysema.query.jpa.impl.JPAQuery;
 import improve.web.improve.model.CatEntity;
 import improve.web.improve.model.QCatEntity;
+import improve.web.improve.services.jpa2.CatService;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+
+
+
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
-import java.util.logging.Logger;
+import java.util.Map;
 
 /**
  * Created by uev on 23.09.16.
@@ -21,6 +28,8 @@ public class SessionFactoryTest extends TestCase {
     protected EntityManager em;
     protected JPQLQuery jpql;
 
+
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -29,7 +38,6 @@ public class SessionFactoryTest extends TestCase {
     }
 
     @Test
-
     public void testSessionFactory() throws Exception {
         QCatEntity qcat = QCatEntity.catEntity;
         em.getTransaction().begin();
@@ -41,5 +49,17 @@ public class SessionFactoryTest extends TestCase {
         em.getTransaction().commit();
         CatEntity res = (CatEntity) jpql.from(qcat).where(qcat.name.eq("test")).singleResult(qcat);
         assertEquals(res.equals(c), true);
+    }
+
+    @Test
+    /**
+     * Overriding some method
+     */
+    public void testGetFilterService() {
+        CatService service = spy(new CatService());
+        doReturn(new JPAQuery(em)).when(service).getJPQLQuery(); // overridng
+        Map<String, String> filter = ImmutableMap.of("product", "Отбеливатель");
+        Object[] resultList =  service.getList(filter);
+        assertEquals(resultList.length > 0, true);
     }
 }
